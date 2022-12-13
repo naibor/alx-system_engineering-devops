17e660dc8ce9289e83d22d42518dba06c07a04b8
naibor <liznaibor.en@gmail.com>
2022-12-13 10:41:51 +0300

    instructions to create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 
 file{'/tmp':
-path     => '/tmp/school',
-mode     => '0744',
-owner    => 'www-data',
-group    => 'www-data',
-content  => 'I love Puppet',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
